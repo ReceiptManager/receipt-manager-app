4e48dcc1c30e5672bb73123a63bafbc3c577bf90
William Todt <william.todt@mailbox.org>
2020-10-01 13:15:00 +0200

    Refactor design and implement widget containers.
    Now, each page has an own widget container. The main.dart is only used to keep track of those.
    Each container has its own implementation in the corresponding widget file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,142 +1,68 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 
-import 'package:async/async.dart';
-import 'package:camera/camera.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:path/path.dart';
-import 'package:path/path.dart' show join;
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+import 'package:receipt_parser/widgets/home.dart';
+import 'package:receipt_parser/widgets/home_old.dart';
+import 'package:receipt_parser/widgets/settings.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import "server.dart";
+SharedPreferences sharedPrefs;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  HttpOverrides.global = new MyHttpOverrides();
-
-  runApp(
-    MaterialApp(theme: ThemeData.dark(), home: ServerForm()
-        //home: TakePictureScreen(
-        //camera: firstCamera,
-        //),
-        ),
-  );
+  sharedPrefs = await SharedPreferences.getInstance();
+  FlutterStatusbarcolor.setStatusBarColor(Colors.white);
+  runApp(MaterialApp(home: HomeScreen()));
 }
 
-class TakePictureScreen extends StatefulWidget {
-  final CameraDescription camera;
-
-  const TakePictureScreen({
-    Key key,
-    @required this.camera,
-  }) : super(key: key);
-
+class HomeScreen extends StatefulWidget {
   @override
-  TakePictureScreenState createState() => TakePictureScreenState();
+  HomeScreenState createState() => HomeScreenState();
 }
 
-class TakePictureScreenState extends State<TakePictureScreen> {
-  CameraController _controller;
-  Future<void> _initializeControllerFuture;
+class HomeScreenState extends State<HomeScreen> {
+  final List<Widget> _children = [
+    HomeWidget(sharedPrefs),
+    PlaceholderWidget(Colors.blueAccent),
+    PlaceholderWidget(Colors.blueAccent),
+    SettingsWidget(sharedPrefs)
+  ];
+
+  int _curent_index = 0;
 
   @override
   void initState() {
     super.initState();
-    _controller = CameraController(
-      widget.camera,
-      ResolutionPreset.ultraHigh,
-    );
-
-    _initializeControllerFuture = _controller.initialize();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Scan receipt')),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera_rounded),
-        backgroundColor: Colors.blue,
-        onPressed: () async {
-          try {
-            await _initializeControllerFuture;
-            final path = join(
-              (await getTemporaryDirectory()).path,
-              '${DateTime.now()}.png',
-            );
-
-            await _controller.takePicture(path);
-            sendImage(File(path));
-
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(imagePath: path),
-              ),
-            );
-          } catch (e) {
-            print(e);
-          }
-        },
-      ),
-    );
-  }
-
-  sendImage(File imageFile) async {
-    var stream =
-    new http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
-    var length = await imageFile.length();
-    var url = "https://192.168.0.103:5000/api/upload/";
-    var request = new http.MultipartRequest("POST", Uri.parse(url));
-    var multipartFile = new http.MultipartFile('file', stream, length,
-        filename: basename(imageFile.path));
-
-    request.files.add(multipartFile);
-    var response = await request.send();
-    print(response.statusCode);
-    response.stream.transform(utf8.decoder).listen((value) {
-      print(value);
-    });
-  }
-}
-
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
-
-  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Receipt')),
-      body: Image.file(File(imagePath)),
-    );
-  }
-}
-
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host,
-          int port) => true;
+        appBar: AppBar(title: Text('Scan receipt')),
+        bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Colors.blueAccent,
+          color: Colors.white,
+          items: <Widget>[
+            Icon(Icons.home, size: 30, color: Colors.black),
+            Icon(Icons.history, size: 30, color: Colors.black),
+            Icon(Icons.pie_chart, size: 30, color: Colors.black),
+            Icon(Icons.settings, size: 30, color: Colors.black),
+          ],
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 600),
+          onTap: (index) {
+            setState(() {
+              this._curent_index = index;
+            });
+          },
+        ),
+        body: _children[_curent_index]);
   }
 }
