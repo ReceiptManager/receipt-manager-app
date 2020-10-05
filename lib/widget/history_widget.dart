0bf13a59804f577de5b26cfd516c43c26db7d13c
William Todt <william.todt@mailbox.org>
2020-10-05 11:19:22 +0200

    Build History and Stats Widget (WIP) The history widget shows all stored receipts and the stats widget shows the corresponding graphs.

---
+++ lib/widget/history_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HistoryWidget extends StatefulWidget {
+  @override
+  _HistoryWidgetState createState() => _HistoryWidgetState();
+}
+
+class _HistoryWidgetState extends State<HistoryWidget> {
+  @override
+  Widget build(BuildContext context) {}
+}
---
+++ lib/widget/stats_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StatsWidget extends StatelessWidget {
+  StatsWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blueAccent);
+  }
+}
