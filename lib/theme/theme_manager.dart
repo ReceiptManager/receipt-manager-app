89fc0848973cbb49ec089c66f0fad44b1a51388a
William Todt <william.todt@mailbox.org>
2020-10-08 15:25:17 +0200

    Update design in the history widget (WIP)
    The new design features an easy way to edit stored receipts. It is based at the design used in home.dart.

---
+++ lib/theme/theme_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThemeManager {
+  static ThemeData getTheme() {
+    return new ThemeData(
+      primaryColor: Colors.white,
+      primaryColorDark: Colors.white,
+    );
+  }
+}
