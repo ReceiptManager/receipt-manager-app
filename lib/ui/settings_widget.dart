df6285c824d5d1b684c48e1c0594b2fdd96ec8b3
William Todt <william.todt@mailbox.org>
2020-10-14 14:27:30 +0200

    Apply new settings screen using the settings flutter library and change default colors.
    The flutter settings library, uses by default a grey background. Even if the background color is set, the library does ignore the color.

--- lib/ui/settings_widget.dart
+++ lib/ui/settings_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:giffy_dialog/giffy_dialog.dart';
 import 'package:receipt_parser/converter/color_converter.dart';
+import 'package:receipt_parser/ui/settings/settings_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 // TODO toast message in settings_widget.dart
@@ -27,79 +28,8 @@ class SettingsWidget extends StatelessWidget {
       );
     }
 
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: new Theme(
-                    data: new ThemeData(
-                      primaryColor: Colors.white,
-                      primaryColorDark: Colors.white,
-                    ),
-                    child: serverTextfield())),
-            new Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: FloatingActionButton(
-                        onPressed: () async {
-                          final ipv4_regex =
-                              "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\$";
-
-                          RegExp ipRegex = new RegExp(ipv4_regex,
-                              caseSensitive: false, multiLine: false);
-
-                          if (ipv4.isEmpty || !ipRegex.hasMatch(ipv4)) {
-                            showDialog(
-                                context: context,
-                                builder: (_) => AssetGiffyDialog(
-                                      image: Image.asset(
-                                        "assets/robot.gif",
-                                        fit: BoxFit.fill,
-                                      ),
-                                      title: Text(
-                                        'Invalid server ip',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            fontSize: 22.0,
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.w600),
-                                      ),
-                                      entryAnimation:
-                                          EntryAnimation.BOTTOM_RIGHT,
-                                      description: Text(
-                                        'The given submitted server ip appear invalid. Please try again.',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(),
-                                      ),
-                                      onCancelButtonPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      onOkButtonPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ));
-                            return;
-                          }
-
-                          sharedPreferences.setString("ipv4", ipv4);
-                          Scaffold.of(context)
-                            ..hideCurrentSnackBar()
-                            ..showSnackBar(SnackBar(
-                              content: Text("Server ip is set."),
-                              backgroundColor: Colors.green,
-                            ));
-                        },
-                        child: Icon(Icons.done_all),
-                        backgroundColor: HexColor.fromHex("#232F34"),
-                        foregroundColor: HexColor.fromHex("#F9AA33"))))
-          ],
-        ),
-      ),
-    );
+    bool value = true;
+    return SettingsScreen();
   }
 
   sendServerAlert(BuildContext _context) {
