7afdfc33ec836031c0c308304b8eae360c8b98ab
William Todt <william.todt@mailbox.org>
2020-10-14 14:18:36 +0200

    Replace BlocDelegate with latest BlocObserver.

--- lib/bloc/delegate/simple_delegate.dart
+++ lib/bloc/delegate/simple_delegate.dart
@@ -1,16 +1,12 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SimpleDelegate extends BlocDelegate {
+
+class SimpleDelegate extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object event) {
     super.onEvent(bloc, event);
     print(event.toString());
-  }
-
-  @override
-  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
-    super.onError(bloc, error, stacktrace);
-    print(error.toString());
   }
 
   @override
