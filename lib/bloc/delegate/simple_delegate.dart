d6ce877eeaaf84589c7317682b4f97d5455018bd
William Todt <william.todt@mailbox.org>
2020-10-17 15:54:35 +0200

    Implement insert method using a ReceiptCompanion.

--- lib/bloc/delegate/simple_delegate.dart
+++ lib/bloc/delegate/simple_delegate.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,6 +12,11 @@ class SimpleDelegate extends BlocObserver {
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
+    if (transition.currentState == null) {
+      log("[TRANSITION] can't follow transaction. State is empty.");
+      return;
+    }
+
     super.onTransition(bloc, transition);
     print(transition.toString());
   }
