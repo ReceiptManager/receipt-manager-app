e407eb042c7b301a3c767979d9eba602a3737898
William Todt <william.todt@mailbox.org>
2020-10-21 21:19:54 +0200

    Implement the bloc provider.
    Now, the bloc provider will get a list receipts and create the corresponding chart.

--- lib/ui/stats_widget.dart
+++ lib/ui/stats_widget.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receipt_parser/bloc/moor/db_bloc.dart';
 import 'package:receipt_parser/bloc/moor/db_state.dart';
+import 'package:receipt_parser/database/receipt_database.dart';
 import 'package:receipt_parser/factory/padding_factory.dart';
 import 'package:receipt_parser/generated/l10n.dart';
 import 'package:receipt_parser/theme/color/color.dart';
@@ -32,78 +34,13 @@ class StatsWidgetState extends State<StatsWidget> {
   final Duration animDuration = const Duration(milliseconds: 250);
   final DbBloc _bloc;
 
+  // store expenses
+  List<double> expenses = [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00];
+
   int touchedIndex;
 
   StatsWidgetState(this._bloc);
 
-  @override
-  void initState() {
-    generateData();
-  }
-
-  /*
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-        create: (_) => _bloc,
-        child: SingleChildScrollView(
-            child: Column(children: <Widget>[
-          PaddingFactory.create(AspectRatio(
-            aspectRatio: 0.8,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18)),
-              color: LightColor.brighter,
-              child: Stack(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        Text(
-                          S.of(context).overview,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(
-                          height: 4,
-                        ),
-                        Text(
-                          S.of(context).overviewExpenses,
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(
-                          height: 38,
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: BarChart(
-                              mainBarData(),
-                              swapAnimationDuration: animDuration,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 12,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ))
-        ])));
-  }
-*/
   @override
   Widget build(BuildContext context) {
     return BlocBuilder(
@@ -121,10 +58,9 @@ class StatsWidgetState extends State<StatsWidget> {
         }
         if (state is LoadedState) {
           final receipt = state.receipt;
-          return Column(
-            children: <Widget>[Expanded(child: _buildList(receipt))],
-          );
+          return _buildList(receipt);
         }
+
         return Container(
             color: Colors.white,
             decoration: BoxDecoration(
@@ -136,10 +72,10 @@ class StatsWidgetState extends State<StatsWidget> {
     );
   }
 
-  _buildList(r) {
-    generateData();
+  SingleChildScrollView _buildList(r) {
+    generateData(r);
 
-    SingleChildScrollView(
+    return SingleChildScrollView(
         child: Column(children: <Widget>[
       PaddingFactory.create(AspectRatio(
         aspectRatio: 0.8,
@@ -198,11 +134,20 @@ class StatsWidgetState extends State<StatsWidget> {
 
   DateTime getDate(DateTime d) => DateTime(d.year, d.month, d.day);
 
-  void generateData() {
+  void generateData(List<Receipt> r) {
     final date = DateTime.now();
     final startDate = getDate(date.subtract(Duration(days: date.weekday - 1)));
-    final endDate =
-        getDate(date.add(Duration(days: DateTime.daysPerWeek - date.weekday)));
+
+    for (Receipt receipt in r) {
+      for (int i = 0; i < 7; i++) {
+        var d = new DateTime(startDate.year, startDate.month, startDate.day + i);
+        if (receipt.date.year == d.year &&
+            receipt.date.month == d.month &&
+            receipt.date.day == d.day) {
+          expenses[i] = double.parse(receipt.total);
+        }
+      }
+    }
   }
 
   BarChartGroupData makeGroupData(
@@ -234,19 +179,19 @@ class StatsWidgetState extends State<StatsWidget> {
   List<BarChartGroupData> showingGroups() => List.generate(7, (i) {
         switch (i) {
           case 0:
-            return makeGroupData(0, 50, isTouched: i == touchedIndex);
+            return makeGroupData(0, expenses[0], isTouched: i == touchedIndex);
           case 1:
-            return makeGroupData(1, 65, isTouched: i == touchedIndex);
+            return makeGroupData(1, expenses[1], isTouched: i == touchedIndex);
           case 2:
-            return makeGroupData(2, 50, isTouched: i == touchedIndex);
+            return makeGroupData(2, expenses[2], isTouched: i == touchedIndex);
           case 3:
-            return makeGroupData(3, 75, isTouched: i == touchedIndex);
+            return makeGroupData(3, expenses[3], isTouched: i == touchedIndex);
           case 4:
-            return makeGroupData(4, 90, isTouched: i == touchedIndex);
+            return makeGroupData(4, expenses[4], isTouched: i == touchedIndex);
           case 5:
-            return makeGroupData(5, 115, isTouched: i == touchedIndex);
+            return makeGroupData(5, expenses[5], isTouched: i == touchedIndex);
           case 6:
-            return makeGroupData(6, 65, isTouched: i == touchedIndex);
+            return makeGroupData(6, expenses[6], isTouched: i == touchedIndex);
           default:
             return null;
         }
