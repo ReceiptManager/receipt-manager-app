a15c3faf35db94b45e17d994dda774f9921ef062
William Todt <william.todt@mailbox.org>
2020-10-24 22:21:51 +0200

    Create picture widget.

--- lib/ui/camera_picker.dart
+++ lib/ui/camera_picker.dart
@@ -24,6 +24,8 @@ import 'package:flutter/material.dart';
 import 'package:image/image.dart' as img;
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:receipt_parser/factory/padding_factory.dart';
 import 'package:receipt_parser/network/network_client.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -67,27 +69,17 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     List<int> imageBytes = await originalFile.readAsBytes();
 
     final originalImage = img.decodeImage(imageBytes);
-
     final height = originalImage.height;
     final width = originalImage.width;
 
-    // Let's check for the image size
-    // This will be true also for upside-down photos but it's ok for me
     if (height >= width) {
-      // I'm interested in portrait photos so
-      // I'll just return here
       return originalFile;
     }
 
-    // We'll use the exif package to read exif data
-    // This is map of several exif properties
-    // Let's check 'Image Orientation'
     final exifData = await readExifFromBytes(imageBytes);
-
     img.Image fixedImage;
 
     if (height < width) {
-      // rotate
       if (exifData['Image Orientation'].printable.contains('Horizontal')) {
         fixedImage = img.copyRotate(originalImage, 90);
       } else if (exifData['Image Orientation'].printable.contains('180')) {
@@ -99,9 +91,6 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       }
     }
 
-    // Here you can select whether you'd like to save it as png
-    // or jpg with some compression
-    // I choose jpg with 100% quality
     final fixedFile =
         await originalFile.writeAsBytes(img.encodeJpg(fixedImage));
 
@@ -124,6 +113,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
         },
       ),
       floatingActionButton: FloatingActionButton(
+        heroTag: "imagePicker",
         child: Icon(
           Icons.camera_alt,
         ),
@@ -140,13 +130,116 @@ class TakePictureScreenState extends State<TakePictureScreen> {
           // await FlutterExifRotation.rotateAndSaveImage(path: path);
           await fixExifRotation(path);
 
-          // Get current ip which is given by the user
-          SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
-          String ip = sharedPrefs.get("ipv4");
-
-          // parse server response and fill form
-          await NetworkClient.sendImage(File(path), ip, context, key);
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: path),
+              ));
         },
+      ),
+    );
+  }
+}
+
+class DisplayPictureScreen extends StatefulWidget {
+  final String imagePath;
+
+  DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => DisplayPictureScreenState(imagePath);
+}
+
+class DisplayPictureScreenState extends State<DisplayPictureScreen> {
+  final String imagePath;
+  final GlobalKey<ScaffoldState> key2 = GlobalKey<ScaffoldState>();
+  Color _acceptButtonColor = Colors.green;
+  Color _declineButtonColor = Colors.red;
+  ProgressDialog pr;
+
+  bool _isButtonDisabled;
+
+  DisplayPictureScreenState(this.imagePath);
+
+  @override
+  void initState() {
+    super.initState();
+    _isButtonDisabled = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    pr = ProgressDialog(
+      context,
+      type: ProgressDialogType.Normal,
+      textDirection: TextDirection.ltr,
+      isDismissible: false,
+    );
+
+    pr.style(
+      message:
+          'Parsing image using tesseract. This takes some time.',
+      borderRadius: 10.0,
+      backgroundColor: Colors.white,
+      elevation: 10.0,
+      insetAnimCurve: Curves.easeInCirc,
+      messageTextStyle: TextStyle(
+          color: Colors.black, fontSize: 15.0, fontWeight: FontWeight.w600),
+    );
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      key: key2,
+      body: Column(
+        children: [
+          PaddingFactory.create(
+              Container(height: 550, child: Image.file(File(imagePath)))),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              PaddingFactory.create(Center(
+                  child: IgnorePointer(
+                ignoring: _isButtonDisabled,
+                child: FloatingActionButton(
+                    heroTag: "decline",
+                    backgroundColor: _declineButtonColor,
+                    child: Icon(
+                      Icons.clear,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+              ))),
+              PaddingFactory.create(Center(
+                  child: IgnorePointer(
+                      ignoring: _isButtonDisabled,
+                      child: FloatingActionButton(
+                          heroTag: "accept",
+                          backgroundColor: _acceptButtonColor,
+                          child: Icon(
+                            Icons.done,
+                          ),
+                          onPressed: _isButtonDisabled
+                              ? null
+                              : () async {
+                                  setState(() {
+                                    _isButtonDisabled = true;
+                                    _acceptButtonColor = Colors.grey;
+                                    _declineButtonColor = Colors.grey;
+                                  });
+                                  SharedPreferences sharedPrefs =
+                                      await SharedPreferences.getInstance();
+                                  String ip = sharedPrefs.get("ipv4");
+
+                                  pr.show();
+                                  await NetworkClient.sendImage(
+                                      File(imagePath), ip, context, key2);
+
+                                  pr.hide();
+                                })))),
+            ],
+          )
+        ],
       ),
     );
   }
