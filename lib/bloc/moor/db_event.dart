b28420270323f75a385a980defbb17413e29e05d
William Todt <william.todt@mailbox.org>
2020-10-27 20:20:16 +0100

    Remove unimplemented methods.

--- lib/bloc/moor/db_event.dart
+++ lib/bloc/moor/db_event.dart
@@ -27,63 +27,54 @@ class StartAppEvent extends DbEvent {
   @override
   String toString() => "StartAppEvent";
 
-  @override
-  List<Object> get props => throw UnimplementedError();
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
-// ignore: must_be_immutable
 class InsertEvent extends DbEvent {
-  ReceiptsCompanion receipt;
+  final ReceiptsCompanion receipt;
 
   InsertEvent({this.receipt}) : super([receipt]);
 
   @override
   String toString() => "InsertEvent : $receipt";
 
-  @override
-  List<Object> get props => throw UnimplementedError();
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
-// ignore: must_be_immutable
 class UpdateEvent extends DbEvent {
-  Receipt receipt;
+  final Receipt receipt;
 
   UpdateEvent({this.receipt}) : super([receipt]);
 
   @override
   String toString() => "UpdateEvent";
 
-  @override
-  List<Object> get props => throw UnimplementedError();
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
 class SwitchButtonEvent extends DbEvent {
   @override
   String toString() => "SwitchButtonEvent";
 
-  @override
-  List<Object> get props => throw UnimplementedError();
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
-// ignore: must_be_immutable
 class DeleteEvent extends DbEvent {
-  Receipt receipt;
+  final Receipt receipt;
 
   DeleteEvent({this.receipt}) : super([receipt]);
 
   @override
   String toString() => "DeleteEvent - $receipt";
 
-  @override
-  List<Object> get props => throw UnimplementedError();
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
 class DeleteAllEvent extends DbEvent {
   @override
   String toString() => "DeleteAllEvent";
 
-  @override
-  List<Object> get props => throw UnimplementedError();
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
 class ReceiptWatchEvent {
@@ -91,11 +82,9 @@ class ReceiptWatchEvent {
   String toString() => "ReceiptWatchEvent";
 }
 
-//notify the bloc fetch all the record form db
 class ReceiptAllFetch extends DbEvent {
   @override
   String toString() => "ReceiptAllFetch";
 
-  @override
-  List<Object> get props => throw UnimplementedError();
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
