67efcd9c3424fd9b4063ce67a7610ca3245ac137
William Todt <william.todt@mailbox.org>
2020-10-30 23:49:25 +0100

    Add filter chip widget.
    The filter chip widget allows to filter the history by clicking at the category chips.
    The color is randomly generated.

--- lib/ui/filter/filter_chips.dart
+++ lib/ui/filter/filter_chips.dart
@@ -1,7 +1,24 @@
+/*
+ *  Copyright (c) 2020 - William Todt
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import 'package:flutter/material.dart';
+import 'package:random_color/random_color.dart';
 import 'package:receipt_manager/factory/categories_factory.dart';
 import 'package:receipt_manager/factory/padding_factory.dart';
-import 'package:random_color/random_color.dart';
 
 
 
@@ -18,20 +35,19 @@ class _FilterChipScreenState extends State<FilterChipScreen> {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: data.length,
-            itemBuilder: (BuildContext context, int index) =>
-                PaddingFactory.create(
-                  FilterChip(
-                    label: Text(data[index].name),
-                    onSelected: (bool value) {
-                      if (selected.contains(index)) {
-                        selected.remove(index);
-                      } else {
+    return Container(
+      height: 75,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: data.length,
+          itemBuilder: (BuildContext context, int index) =>
+              PaddingFactory.create(
+                FilterChip(
+                  label: Text(data[index].name),
+                  onSelected: (bool value) {
+                    if (selected.contains(index)) {
+                      selected.remove(index);
+                    } else {
                         selected.add(index);
                       }
                       setState(() {});
@@ -44,7 +60,6 @@ class _FilterChipScreenState extends State<FilterChipScreen> {
                     backgroundColor: _rand.randomColor(colorHue: ColorHue.blue),
                   ),
                 )),
-      ),
     );
   }
 }
--- lib/ui/receipt_form.dart
+++ lib/ui/receipt_form.dart
@@ -1,15 +1,16 @@
 /*
- * Copyright (c) 2020 - William Todt
+ *  Copyright (c) 2020 - William Todt
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -332,9 +333,9 @@ class ReceiptInputController extends State<ReceiptForm> {
   FloatingActionButton submitButton() {
     return new FloatingActionButton(
         onPressed: () {
-          final form = _formKey.currentState;
-          if (form.validate() &&
-              receiptCategory != null) {
+          //final form = _formKey.currentState;
+          // disable form validation for now
+          if (receiptCategory != null) {
             try {
               Scaffold.of(context).showSnackBar(SnackBar(
                 content: Text(S.of(context).addReceipt),
