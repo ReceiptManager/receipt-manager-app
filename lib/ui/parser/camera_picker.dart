f0c1edfdec5329eb5ea09fe29d71996d1b149972
William Todt <william.todt@mailbox.org>
2020-11-20 11:54:39 +0100

    Refactor camera picker

--- lib/ui/parser/camera_picker.dart
+++ lib/ui/parser/camera_picker.dart
@@ -28,6 +28,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:progress_dialog/progress_dialog.dart';
 import 'package:receipt_manager/factory/padding_factory.dart';
 import 'package:receipt_manager/network/network_client.dart';
+import 'package:receipt_manager/ui/parser/display_image.dart';
 import 'package:rounded_loading_button/rounded_loading_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -103,127 +104,6 @@ class TakePictureScreenState extends State<TakePictureScreen> {
                 builder: (context) => DisplayPictureScreen(imagePath: path),
               ));
         },
-      ),
-    );
-  }
-}
-
-class DisplayPictureScreen extends StatefulWidget {
-  final String imagePath;
-
-  DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() => DisplayPictureScreenState(imagePath);
-}
-
-class DisplayPictureScreenState extends State<DisplayPictureScreen> {
-  final String imagePath;
-  final GlobalKey<ScaffoldState> key2 = GlobalKey<ScaffoldState>();
-  Color _acceptButtonColor = Colors.green;
-  Color _declineButtonColor = Colors.red;
-  double _progress = 0.0;
-  ProgressDialog pr;
-
-  bool _isButtonDisabled;
-
-  DisplayPictureScreenState(this.imagePath);
-
-  @override
-  void initState() {
-    super.initState();
-    _isButtonDisabled = false;
-  }
-
-  Future<File> fixExifRotation(String imagePath) async {
-    final originalFile = File(imagePath);
-    List<int> imageBytes = await originalFile.readAsBytes();
-
-    final originalImage = img.decodeImage(imageBytes);
-    final height = originalImage.height;
-    final width = originalImage.width;
-
-    if (height >= width) {
-      return originalFile;
-    }
-
-    final exifData = await readExifFromBytes(imageBytes);
-    img.Image fixedImage;
-
-    if (height < width) {
-      if (exifData['Image Orientation'].printable.contains('Horizontal')) {
-        fixedImage = img.copyRotate(originalImage, 90);
-      } else if (exifData['Image Orientation'].printable.contains('180')) {
-        fixedImage = img.copyRotate(originalImage, -90);
-      } else if (exifData['Image Orientation'].printable.contains('CCW')) {
-        fixedImage = img.copyRotate(originalImage, 180);
-      } else {
-        fixedImage = img.copyRotate(originalImage, 0);
-      }
-    }
-
-    final fixedFile =
-        await originalFile.writeAsBytes(img.encodeJpg(fixedImage));
-    return fixedFile;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final RoundedLoadingButtonController _btnController =
-        new RoundedLoadingButtonController();
-
-    return Scaffold(
-      appBar: AppBar(title: Text('Display the Picture')),
-      key: key2,
-      body: Column(
-        children: [
-          Center(
-              child: PaddingFactory.create(Container(
-                  height: MediaQuery.of(context).size.height / 1.5,
-                  child: Image.file(File(imagePath))))),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              PaddingFactory.create(Center(
-                  child: IgnorePointer(
-                      ignoring: _isButtonDisabled,
-                      child: RoundedLoadingButton(
-                          width: 60,
-                          height: 60,
-                          child: Icon(Icons.clear, color: Colors.white),
-                          animateOnTap: true,
-                          color: _declineButtonColor,
-                          controller: _btnController,
-                          onPressed: () {
-                            Navigator.pop(context);
-                          })))),
-              PaddingFactory.create(Center(
-                  child: IgnorePointer(
-                      ignoring: _isButtonDisabled,
-                      child: RoundedLoadingButton(
-                          width: 60,
-                          height: 60,
-                          child: Icon(Icons.done, color: Colors.white),
-                          animateOnTap: true,
-                          color: _acceptButtonColor,
-                          controller: _btnController,
-                          onPressed: () async {
-                            setState(() {
-                              _isButtonDisabled = true;
-                              _declineButtonColor = Colors.grey;
-                            });
-                            SharedPreferences sharedPrefs =
-                                await SharedPreferences.getInstance();
-                            String ip = sharedPrefs.get("ipv4");
-
-                            //await fixExifRotation(imagePath);
-                            await NetworkClient.sendImage(
-                                File(imagePath), ip, context, key2);
-                            _progress = _progress + 80.0;
-                          })))),
-            ],
-          )
-        ],
       ),
     );
   }
---
+++ lib/ui/settings/open_source.dart
@@ -0,0 +1,87 @@
+/*
+ *  Copyright (c) 2020 - William Todt
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:receipt_manager/generated/l10n.dart';
+import 'package:receipt_manager/theme/color/color.dart';
+import 'package:settings_ui/settings_ui.dart';
+
+class OpenSourceSettings extends StatefulWidget {
+  @override
+  _OpenSourceSettingsState createState() => _OpenSourceSettingsState();
+}
+
+class _OpenSourceSettingsState extends State<OpenSourceSettings> {
+  var opensource_licenses = [
+    "camera: ^0.5.8+9",
+    "path_provider:",
+    "path: 1.1.0",
+    "curved_navigation_bar: ^0.3.4"
+        "shared_preferences:  ^0.5.12+2",
+    "giffy_dialog: ^1.8.0",
+    "flutter_bloc: ^6.0.6",
+    "equatable: ^1.2.5",
+    "moor_flutter: ^3.1.0",
+    "flutter_slidable: ^0.5.7",
+    "moor: ^3.3.1",
+    "cupertino_icons: ^1.0.0",
+    "intl: ^0.16.1",
+    "settings_ui: ^0.4.0",
+    "flutter_staggered_grid_view: ^0.3.2",
+    "permission_handler: ^5.0.1+1",
+    "fl_chart: ^0.12.0",
+    "flutter_exif_rotation: ^0.3.1",
+    "image: ^2.1.18",
+    "exif: ^1.0.3",
+    "progress_dialog: ^1.2.4",
+    "rounded_loading_button: ^1.0.0",
+    "simple_autocomplete_formfield: ^0.2.7",
+    "flutter_onboard: ^0.1.0",
+    "provider: ^4.3.2+2",
+    "random_color: ^1.0.5",
+    "simple_animations: ^2.2.2",
+    "random_date: ^0.0.5",
+  ];
+
+  @override
+  void initState() {
+    log(Intl.getCurrentLocale());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(S.of(context).openSourceLicence)),
+        body: ListView.builder(
+            itemCount: opensource_licenses.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                margin: EdgeInsets.all(4),
+                color: LightColor.brighter,
+                child: Center(
+                    child: Text(
+                  '${opensource_licenses[index]}',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                )),
+              );
+            }));
+  }
+}
