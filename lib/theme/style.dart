a90e5d123b9d1b5eb0860025451665d5a3785ba7
William Todt <william.todt@mailbox.org>
2020-12-18 09:42:32 +0100

    Add dashboard text style

---
+++ lib/theme/style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const buttonTextStyle = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const chartLabelsTextStyle = TextStyle(
+    color: Colors.grey,
+    fontSize: 14.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const tabTextStyle = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+  );
+}
