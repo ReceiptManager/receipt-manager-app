b35304e1bb9702e32d8e4bad39e842daa334bcf6
William Todt <william.todt@mailbox.org>
2021-02-11 13:06:35 +0100

    [CORE] Create receipt memento to store receipts

---
+++ lib/memento/receipt_memento.dart
@@ -0,0 +1,22 @@
+import 'dart:core';
+
+import 'package:receipt_manager/database/receipt_database.dart';
+
+class ReceiptMemento {
+  static final ReceiptMemento _receiptShare = ReceiptMemento._internal();
+
+  List<Receipt> receipts;
+  List<Receipt> finalReceipts;
+
+  factory ReceiptMemento() {
+    return _receiptShare;
+  }
+
+  ReceiptMemento._internal();
+
+  void apply(List<Receipt> receipts) {
+    this.finalReceipts = receipts;
+    this.receipts = receipts;
+  }
+
+}
