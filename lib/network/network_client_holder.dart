933e8190012601fb4c78fc5e14be57fb7988cc73
William Todt <william.todt@mailbox.org>
2021-02-20 00:11:33 +0100

    [CORE] parse network client options in network client holder and submit the arguments via network client holder

---
+++ lib/network/network_client_holder.dart
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2020 - 2021 : William Todt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import 'dart:io';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// The [NetworkClientHolder] is used to store and update submitted
+/// arguments submitted by the user.
+/// All arguments get previously stored in shared preferences.
+/// The NetworkClientHolder first read the options and submit them
+/// to the network client.
+class NetworkClientHolder {
+  /// The image file name
+  File imageFile;
+
+  /// Receipt server ip
+  String ip;
+
+  /// Receipt server api token
+  String token;
+
+  /// Show debug output in alert boxes
+  bool sendDebugOutput;
+
+  /// Grayscale image
+  bool grayscale;
+
+  /// Apply gaussian filter to image
+  bool gaussian;
+
+  /// Use legacy parser to extract text
+  bool legacyParser;
+
+  /// Rotate image by 90°
+  bool rotate;
+
+  /// Show receipt article list
+  bool showItemList;
+
+  SharedPreferences sharedPrefs;
+
+  static final NetworkClientHolder _networkClientHolder =
+      NetworkClientHolder._internal();
+
+  NetworkClientHolder._internal();
+  factory NetworkClientHolder() {
+    return _networkClientHolder;
+  }
+  void readOptions() async {
+    sharedPrefs = await SharedPreferences.getInstance();
+    ip = sharedPrefs.get("ipv4");
+    token = sharedPrefs.get("api_token");
+    sendDebugOutput = sharedPrefs.get("enable_debug_output");
+    grayscale = sharedPrefs.get("grayscale");
+    gaussian = sharedPrefs.get("gaussian");
+    legacyParser = sharedPrefs.get("legacyParser");
+    rotate = sharedPrefs.get("rotate");
+    showItemList = sharedPrefs.get("showItemList");
+  }
+}
