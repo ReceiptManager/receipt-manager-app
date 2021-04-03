9026fc985a476a31bb5121b4b04a8ea26d68e733
William Todt <william.todt@mailbox.org>
2021-04-03 23:56:18 +0200

    [CORE] [WIP] implement StatsControllerWidget and StatsViewWidget

--- lib/app/pages/stats/stat_view.dart
+++ lib/app/pages/stats/stat_view.dart
@@ -15,13 +15,35 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+import 'package:receipt_manager/app/pages/stats/stat_controller.dart';
 
 class StatsPage extends View {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
+  State<StatefulWidget> createState() => StatsState();
+}
+
+class StatsState extends ViewState<StatsPage, StatsController> {
+  StatsState() : super(StatsController());
+
+  @override
+  Widget get view => Scaffold(
+        key: globalKey,
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: ControlledWidgetBuilder<StatsController>(
+                  builder: (context, controller) {
+                return Container(width: 0.0, height: 0.0);
+              }),
+            ),
+            ControlledWidgetBuilder<StatsController>(
+                builder: (context, controller) {
+              return Container(width: 0.0, height: 0.0);
+            }),
+          ],
+        ),
+      );
 }
