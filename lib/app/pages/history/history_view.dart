11ad4302a1bb51d9f7a8d4a7d90082b4614909ca
William Todt <william.todt@mailbox.org>
2021-04-12 08:45:23 +0200

    [CORE] Use neumorphic app bar

--- lib/app/pages/history/history_view.dart
+++ lib/app/pages/history/history_view.dart
@@ -18,11 +18,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:receipt_manager/app/pages/history/history_controller.dart';
-import 'package:receipt_manager/app/widgets/banner_widget.dart';
-import 'package:receipt_manager/app/widgets/padding_widget.dart';
-import 'package:receipt_manager/app/widgets/slidable_widet.dart';
+import 'package:receipt_manager/app/widgets/padding/padding_widget.dart';
+import 'package:receipt_manager/app/widgets/slideable/slidable_widet.dart';
+import 'package:receipt_manager/data/repository/data_receipts_repository.dart';
 
 class HistoryPage extends View {
   @override
@@ -30,15 +31,14 @@ class HistoryPage extends View {
 }
 
 class HistoryState extends ViewState<HistoryPage, HistoryController> {
-  HistoryState() : super(HistoryController());
+  HistoryState() : super(HistoryController(DataReceiptRepository()));
 
   Widget weeklyOverview(BuildContext context) =>
       ControlledWidgetBuilder<HistoryController>(
           builder: (context, controller) {
-        if (controller.getReceipts.length == 0) return Container();
+        if (controller.receipts.length == 0) return Container();
 
         return Container(
-            color: Colors.white,
             child: Align(
                 alignment: Alignment.centerRight,
                 child: PaddingWidget(
@@ -57,7 +57,7 @@ class HistoryState extends ViewState<HistoryPage, HistoryController> {
               (BuildContext context, ScrollController scrollController) {
             return ListView.builder(
                 padding: const EdgeInsets.all(8.0),
-                itemCount: controller.getReceipts.length,
+                itemCount: controller.receipts.length,
                 itemBuilder: (_, index) {
                   return AnimationConfiguration.staggeredList(
                       position: index,
@@ -71,27 +71,21 @@ class HistoryState extends ViewState<HistoryPage, HistoryController> {
                                   editText: "Edit",
                                   editMethod: controller.editMethod,
                                   imagePath: "assets/lidl.png",
-                                  receipt: controller.getReceipts[index]))));
+                                  receipt: controller.receipts[index]))));
                 });
           }),
         ));
       });
 
-  Widget titlePage() =>
-      Center(child: ControlledWidgetBuilder<HistoryController>(
-          builder: (context, controller) {
-        return BannerWidget(
-          text: "Receipt overview",
-        );
-      }));
-
   @override
   Widget get view => Scaffold(
-        backgroundColor: Colors.white,
         key: globalKey,
+        backgroundColor: Color(0xFFEFEFF4),
+        appBar: NeumorphicAppBar(
+          title: Text("Receipt overview"),
+        ),
         body: Column(
           children: <Widget>[
-            titlePage(),
             weeklyOverview(context),
             receiptVisualisation(context)
           ],
