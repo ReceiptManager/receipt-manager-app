d897e345b94e01e85592b503d2c22e792e5a1762
William Todt <william.todt@mailbox.org>
2021-04-14 11:54:24 +0200

    [CORE] Implement debug list and safe assets loader

--- lib/app/pages/history/history_view.dart
+++ lib/app/pages/history/history_view.dart
@@ -17,15 +17,18 @@
 
 import 'dart:core';
 
+import 'package:animated_stack/animated_stack.dart' as stacked;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:receipt_manager/app/pages/history/history_controller.dart';
+import 'package:receipt_manager/app/widgets/icon/icon_tile.dart';
 import 'package:receipt_manager/app/widgets/padding/padding_widget.dart';
 import 'package:receipt_manager/app/widgets/slideable/slidable_widet.dart';
 import 'package:receipt_manager/data/repository/data_receipts_repository.dart';
+import 'package:receipt_manager/data/storage/receipt_database.dart';
 import 'package:receipt_manager/data/storage/scheme/holder_table.dart';
 
 class HistoryPage extends View {
@@ -64,6 +67,7 @@ class HistoryState extends ViewState<HistoryPage, HistoryController> {
               }
 
               final receipts = snapshot.data ?? [];
+
               return Expanded(
                   child: ListView.builder(
                       padding: const EdgeInsets.all(8.0),
@@ -83,18 +87,142 @@ class HistoryState extends ViewState<HistoryPage, HistoryController> {
                                         editText: "Edit",
                                         editMethod:
                                             controller.editMethod(receipt),
-                                        imagePath: "assets/lidl.png",
-                                        receiptHolder: receipts[index]))));
+                                        image: Image.asset(
+                                          "assets/lidl.png",
+                                          fit: BoxFit.fill,
+                                        ),
+                                        receipt: receipt))));
                       }));
             });
       });
 
+  Image? imageExists(String path) {
+    Image image;
+    try {
+      image = Image.asset(
+        path,
+        fit: BoxFit.fill,
+      );
+    } catch (_) {
+      return null;
+    }
+
+    return image;
+  }
+
+  Image getAssetImage(String storeName, String categoryName) {
+    String storeNamePath =
+        "assets/" + storeName.split(" ")[0].trim().toLowerCase();
+
+    List<String> extentions = [".png", "jpeg", "jpg"];
+
+    for (var ext in extentions) {
+      final String path = storeNamePath + ext;
+      Image? image = imageExists(path);
+      if (image != null) return image;
+    }
+
+    String categoryPath =
+        "assets/" + categoryName.split(" ")[0].trim().toLowerCase();
+    for (var ext in extentions) {
+      final String path = categoryPath + ext;
+      Image? image = imageExists(path);
+      if (image != null) return image;
+    }
+
+    return imageExists("assets/fallback.png")!;
+  }
+
+  Widget historyWidget() {
+    List<Receipt> receipts = [];
+
+    receipts.add(Receipt(
+        total: 39.90, date: DateTime.now(), storeName: "Kaufland", id: 1));
+
+    receipts.add(Receipt(
+        total: 195.44, date: DateTime.now(), storeName: "Apotheke", id: 1));
+
+    receipts.add(
+        Receipt(total: 5.44, date: DateTime.now(), storeName: "Lidl", id: 1));
+
+    receipts.add(
+        Receipt(total: 65.44, date: DateTime.now(), storeName: "Netto", id: 1));
+
+    receipts.add(
+        Receipt(total: 15.44, date: DateTime.now(), storeName: "Edeka", id: 1));
+
+    receipts.add(
+        Receipt(total: 15.44, date: DateTime.now(), storeName: "Edeka", id: 1));
+
+    receipts.add(
+        Receipt(total: 15.44, date: DateTime.now(), storeName: "df", id: 1));
+
+    receipts.add(
+        Receipt(total: 15.44, date: DateTime.now(), storeName: "Edeka", id: 1));
+
+    return ControlledWidgetBuilder<HistoryController>(
+        builder: (context, controller) {
+      return Expanded(
+          child: ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              itemCount: receipts.length,
+              itemBuilder: (_, index) {
+                final receipt = receipts[index];
+                return AnimationConfiguration.staggeredList(
+                    position: index,
+                    duration: const Duration(milliseconds: 375),
+                    child: SlideAnimation(
+                        verticalOffset: 50.0,
+                        child: FadeInAnimation(
+                            child: SlidableHistoryWidget(
+                                deleteText: "Delete",
+                                deleteMethod: controller.deleteMethod(receipt),
+                                editText: "Edit",
+                                editMethod: controller.editMethod(receipt),
+                                image: getAssetImage(receipt.storeName, ""),
+                                receipt: receipts[index]))));
+              }));
+    });
+  }
+
   @override
-  Widget get view => Scaffold(
-      key: globalKey,
-      backgroundColor: Color(0xFFEFEFF4),
-      appBar: NeumorphicAppBar(
-        title: Text("Receipt overview"),
+  Widget get view => stacked.AnimatedStack(
+      backgroundColor: Colors.transparent,
+      fabBackgroundColor: Colors.red,
+      buttonIcon: Icons.filter_list,
+      fabIconColor: Colors.white,
+      animateButton: true,
+      foregroundWidget: Scaffold(
+          key: globalKey,
+          backgroundColor: Color(0xFFEFEFF4),
+          appBar: NeumorphicAppBar(title: Text("Receipt overview")),
+          body: Column(children: [historyWidget()])),
+      columnWidget: Column(
+        children: <Widget>[
+          SizedBox(height: 20),
+          IconTile(
+            width: 60,
+            height: 60,
+            iconData: Icons.insert_drive_file_outlined,
+            fun: () {},
+          ),
+          SizedBox(height: 20),
+          IconTile(
+            width: 60,
+            height: 60,
+            iconData: Icons.camera_alt,
+            fun: () {},
+          ),
+        ],
       ),
-      body: Column(children: [receiptVisualisation(context)]));
+      bottomWidget: Container(
+        decoration: BoxDecoration(
+          color: Color(0xFFEFEFF4),
+          borderRadius: BorderRadius.all(
+            Radius.circular(50),
+          ),
+        ),
+        width: 260,
+        height: 50,
+      ));
 }
