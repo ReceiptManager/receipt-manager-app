3de8821ab84f72a20233d319db84d5223c60b4ee
William Todt <william.todt@mailbox.org>
2021-04-20 11:53:33 +0200

    [CORE] implement api token settings widget

---
+++ lib/app/pages/settings/api_token/api_token_controller.dart
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2020 - 2021 : William Todt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:receipt_manager/data/repository/data_receipts_repository.dart';
+
+import 'api_token_presenter.dart';
+
+class ApiTokenController extends Controller {
+  final ApiTokenPresenter _historyPresenter;
+
+  DataReceiptRepository repository;
+
+  ApiTokenController(DataReceiptRepository repository)
+      : _historyPresenter = ApiTokenPresenter(repository),
+        this.repository = repository,
+        super();
+
+  TextEditingController _apiTokenController = TextEditingController();
+
+  TextEditingController get apiTokenController => _apiTokenController;
+
+  @override
+  void initListeners() {}
+
+  @override
+  void onResumed() => print('On resumed');
+
+  @override
+  void onReassembled() => print('View is about to be reassembled');
+
+  @override
+  void onDeactivated() => print('View is about to be deactivated');
+
+  @override
+  void onDisposed() {
+    _historyPresenter.dispose();
+    super.onDisposed();
+  }
+}
---
+++ lib/app/pages/settings/api_token/api_token_view.dart
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2020 - 2021 : William Todt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import 'dart:core';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:receipt_manager/app/pages/settings/api_token/api_token_controller.dart';
+import 'package:receipt_manager/app/widgets/padding/padding_widget.dart';
+import 'package:receipt_manager/app/widgets/textfield/simple_textfield.dart';
+import 'package:receipt_manager/data/repository/data_receipts_repository.dart';
+
+class ApiTokenPage extends View {
+  @override
+  State<StatefulWidget> createState() => ApiTokenState();
+}
+
+class ApiTokenState extends ViewState<ApiTokenPage, ApiTokenController> {
+  ApiTokenState() : super(ApiTokenController(DataReceiptRepository()));
+
+  Widget apiTextField(ApiTokenController controller) => PaddingWidget(
+          widget: SimpleTextfieldWidget(
+        controller: controller.apiTokenController,
+        hintText: "API Token",
+        labelText: "API Token",
+        helperText: "The API Token",
+        validator: (value) => null,
+        icon: Icon(Icons.vpn_key_sharp),
+        readOnly: false,
+      ));
+
+  Widget submitButton() => PaddingWidget(
+          widget: Align(
+        alignment: Alignment.centerRight,
+        child: NeumorphicButton(
+            style: NeumorphicStyle(
+              shape: NeumorphicShape.flat,
+              boxShape: NeumorphicBoxShape.stadium(),
+            ),
+            child:
+                Text("Submit", style: TextStyle(fontWeight: FontWeight.bold))),
+      ));
+
+  @override
+  Widget get view => Scaffold(
+      key: globalKey,
+      backgroundColor: Colors.white,
+      appBar: NeumorphicAppBar(title: Text("Api Token Settings")),
+      body: ControlledWidgetBuilder<ApiTokenController>(
+          builder: (context, controller) {
+        return Column(children: [apiTextField(controller), submitButton()]);
+      }));
+}
