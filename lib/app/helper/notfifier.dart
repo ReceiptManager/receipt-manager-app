5a3556e4facf3ac4d7dd510d80d6c017a4b8a80e
William Todt <william.todt@mailbox.org>
2021-05-14 23:07:52 +0200

    Add user notification msg method

--- lib/app/helper/notfifier.dart
+++ lib/app/helper/notfifier.dart
@@ -15,6 +15,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class UserNotifier {
@@ -32,5 +34,42 @@ class UserNotifier {
       content: Text(msg),
       backgroundColor: Colors.green,
     ));
+  }
+
+  static msg(String title, String body) async {
+    await AwesomeNotifications().createNotification(
+        content: NotificationContent(
+            id: 120,
+            hideLargeIconOnExpand: false,
+            locked: false,
+            notificationLayout: NotificationLayout.Inbox,
+            channelKey: 'receipt_manager_channel',
+            title: title,
+            body: body),
+        actionButtons: [
+          NotificationActionButton(
+              key: 'EDIT',
+              label: 'Editieren',
+              buttonType: ActionButtonType.Default),
+        ]);
+  }
+
+  static Future<void> progress(final int progress) async {
+    await AwesomeNotifications().createNotification(
+        content: NotificationContent(
+            id: 100,
+            channelKey: "receipt_manager_channel",
+            title: "Upload receipt",
+            notificationLayout: NotificationLayout.ProgressBar,
+            progress: progress,
+            locked: false));
+    if (progress == 100)
+      await AwesomeNotifications().createNotification(
+          content: NotificationContent(
+              id: 110,
+              notificationLayout: NotificationLayout.Default,
+              channelKey: 'receipt_manager_channel',
+              title: 'Kassenbeleg wurde erfolgreich hochgeladen',
+              body: 'Dein Kassenbeleg wird gerade bearbeitet.'));
   }
 }
